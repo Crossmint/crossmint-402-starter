21d750c1120782acd4a36152bc6858a63f4d955c
Angela Gilhotra <angelagilhotra@gmail.com>
2025-10-02 20:46:44 +0530

    fix

--- ping-crossmint/client/src/constants/config.ts
+++ ping-crossmint/client/src/constants/config.ts
@@ -3,6 +3,6 @@ import type { Config } from '../types';
 export const DEFAULT_CONFIG: Config = {
     testEmail: "angela.temp+demo13@example.com",
     chain: "base-sepolia",
-    serverUrl: "https://ping-crossmint-server.vercel.app/",
+    serverUrl: "https://ping-crossmint-server.vercel.app",
     signerType: "api-key",
 };
