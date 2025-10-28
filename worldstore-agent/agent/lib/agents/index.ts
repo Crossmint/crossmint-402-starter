019ef73e5ee64cf1947eeea85159c82787625262
Angela Gilhotra <angelagilhotra@gmail.com>
2025-10-28 18:42:49 +0530

    Move worldstore-agent submodule from external/ to root directory

--- worldstore-agent/agent/lib/agents/index.ts
+++
@@ -1,2 +0,0 @@
-export { createShoppingAgent } from "./shopping/agent";
-export { createProfileAgent } from "./profile/agent";
--- worldstore-agent/worldstore-agent/agent/helpers/loadUserOrders.ts
+++
@@ -1,8 +0,0 @@
-import { UserProfile } from "lib/types";
-import { loadUserOrders as redisLoadUserOrders } from "services/redis";
-
-export const loadUserOrders = async (
-  inboxId: string
-): Promise<UserProfile["orderHistory"]> => {
-  return redisLoadUserOrders(inboxId);
-};
